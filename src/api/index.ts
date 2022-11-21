b846e055ac2c76c1a0a4deb17f9e69bf3345cec7
Alex Young <alex@alexyoung.info>
2022-11-21 00:06:46 +0000

    refactor: 💡 use supertokens instead of auth0 (#3)

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,34 +4,28 @@ import { ApiResponse, ApiResult } from "./types";
 
 export interface MatchFeesApiOptions {
   basePath: string;
-  accessToken: string;
 }
 
 export class MatchFeesApi {
   private readonly apiClient: DefaultApi;
-  private readonly accessToken: string;
 
-  constructor({ basePath, accessToken }: MatchFeesApiOptions) {
+  constructor({ basePath }: MatchFeesApiOptions) {
     this.apiClient = new DefaultApi(new Configuration({ basePath }));
-    this.accessToken = accessToken;
   }
 
   public get user() {
-    return new MatchFeesUserApi(this.apiClient, this.accessToken);
+    return new MatchFeesUserApi(this.apiClient);
   }
 }
 
 class MatchFeesUserApi {
   private readonly requestRunner: RequestRunner = new RequestRunner();
 
-  constructor(
-    private readonly apiClient: DefaultApi,
-    private readonly accessToken: string
-  ) {}
+  constructor(private readonly apiClient: DefaultApi) {}
 
   public async getMe() {
     return this.requestRunner.runApiRequest(() =>
-      this.apiClient.userControllerGetMe(`Bearer ${this.accessToken}`)
+      this.apiClient.userControllerGetMe()
     );
   }
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,20 @@
 const getEnv = (val: string | undefined, name: string) => {
-    if (!val) {
-        throw new Error(`Env var ${name} is not defined`)
-    }
+  if (!val) {
+    throw new Error(`Env var ${name} is not defined`);
+  }
 
-    return val
-}
+  return val;
+};
 
-export const GATSBY_API_AUDIENCE = getEnv(process.env.GATSBY_API_AUDIENCE, "GATSBY_API_AUDIENCE")
-export const GATSBY_API_URL = getEnv(process.env.GATSBY_API_URL, "GATSBY_API_URL")
-export const GATSBY_AUTH_CLIENT_ID = getEnv(process.env.GATSBY_AUTH_CLIENT_ID, "GATSBY_AUTH_CLIENT_ID")
-export const GATSBY_AUTH_DOMAIN = getEnv(process.env.GATSBY_AUTH_DOMAIN, "GATSBY_AUTH_DOMAIN")
-
+export const GATSBY_APP_DOMAIN = getEnv(
+  process.env.GATSBY_APP_DOMAIN,
+  "GATSBY_APP_DOMAIN"
+);
+export const GATSBY_API_DOMAIN = getEnv(
+  process.env.GATSBY_API_DOMAIN,
+  "GATSBY_API_DOMAIN"
+);
+export const GATSBY_AUTH_DOMAIN = getEnv(
+  process.env.GATSBY_AUTH_DOMAIN,
+  "GATSBY_AUTH_DOMAIN"
+);
