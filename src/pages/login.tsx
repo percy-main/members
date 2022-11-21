2cff3400eae0858967b6818c29033968af736ab8
Alex Young <alex.young@clinchd.com>
2022-11-21 00:37:51 +0000

    fix: 🐛 last chance for gatsby

---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { HeadFC } from "gatsby";
+
+const LoginPage = () => {
+  return;
+};
+
+export default LoginPage;
+
+export const Head: HeadFC = () => <title>Login to PMCC</title>;
